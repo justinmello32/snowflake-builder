116022f459674664997e53918d9e538e16a6d5c5
Justin Mello <mello23@llnl.gov>
2021-01-17 11:09:43 -0800

    Initial Commit.

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # snowflake-builder
+Motivational idea:  When we simulate a snowflake we know one macroscopic physical rule for six-fold symmetry.  While governed by the same physical processes, the growth of the snowflake on the microscopic scale is responding to unknown subtle initial and temporal forcing functions.  To solve this problem, we model the initial and forcing function as a random process.  By empirically adjusting the parameters of the random process we can get results that on a macro scale agree pretty well with nature.  
+
+ 
+
+Scientists and engineers do this to model many complex physical systems in nature.
+
+ 
+
+We also do this a lot to make computer-generated images resemble  shapes, motions and patterns in nature that are indistinguishable from natural images.
+
+ 
+
+The  Game:  Use a computer code and adjust random parameters to generate images that look like real snowflakes in nature.
+
+ 
+
+ 
+
+--------------
+
+ 
+
+Here is what you should do to get started:
+
+ 
+
+0. Read a few short articles on the formation of snowflakes in nature.
+
+ 
+
+1.  Install the PROCESSING API from     https://processing.org/download/
+
+ 
+
+That will give you ability to execute processing in both Java and javascript.  Java is an older version but legacy scripts are written in Java.   We only want to use javascript (the JS library is p5.js) but we may get ideas from the legacy codes.
+
+ 
+
+2.  Go to this URL    https://www.openprocessing.org/sketch/492405  in openprocessing.org
+
+ 
+
+When you press the key "o" you have options.
+
+The p5.js source code is also at the same URL
+
+ 
+
+Study the code and learn what each option does.  
+
+ 
+
+3.  Find and study other codes for generating snowflake images
+
+ 
+
+4,  Find at least 10 quality images of real snowflakes.to act as target images in the game.
+
+ 
+
+5.  Modify the JS code in step 2 to have an option to draw just one arm of the snowflake and then render all six arms of the snowflake with the same random set.
---
+++ sketch.js
@@ -0,0 +1,135 @@
+//Quest SnowFlake Project
+
+//Initial Variables
+
+//generation parameters
+let symmetrieNumber = 6; //how many arms the snowflake has (typically 6)
+let angleVarianzPIDivider = 1; //PI is divided by this number to define the variance in branching angles
+let radius = 200; //randius of the snowflake
+let endLength = 2; //length of the branch at which recursions stops
+
+//drawing parameters
+let thickness = 6; //thicknes of the lines
+let thicknesIsLengthDependent = false; //is the stroke thicknes dependent on the lenght of the branch?
+let thicknesFactor = 0.005; //if the stroke thicknes is dependent on the lenght of the branch, this is multiplied with the length which is multiplied with the thickness
+let strokeAlpha = 25; //alpha of the lines
+
+//menu
+let showMenu = true;
+
+let parentDiv;
+let radiusSlider;
+let angleVarianzSlider;
+let endLengthInput;
+
+let thicknessInput;
+let strokeAlphaSlider;
+let thicknesLengthToggle;
+let thicknessLengthSlider;
+
+let generateButton;
+
+//Setup Canvas
+function setup() {
+	createCanvas(1200,800);
+	generateMenu();
+	generateSnowflake();
+}
+
+
+
+function generateMenu() {
+	parentDiv = createDiv('');
+	parentDiv.style('color', 'white');
+
+	radiusSliderP = createP('Radius ');
+    radiusSliderP.parent(parentDiv);
+    radiusSlider = createSlider(0, width / 2, width / 2.5, 1);
+    radiusSlider.parent(radiusSliderP);
+
+    angleVarianzSliderP = createP('Angle Varianz ');
+    angleVarianzSliderP.parent(parentDiv);
+    angleVarianzSlider = createSlider(1, 10, 0.1, 1);
+    angleVarianzSlider.parent(angleVarianzSliderP);
+
+    endLenghtP = createP('End Length ');
+    endLenghtP.parent(parentDiv);
+    endLengthInput = createInput('2');
+    endLengthInput.parent(endLenghtP);
+
+    strokeAlphaSliderP = createP('Line Alpha ');
+    strokeAlphaSliderP.parent(parentDiv);
+    strokeAlphaSlider = createSlider(0, 255, 25, 1);
+    strokeAlphaSlider.parent(strokeAlphaSliderP);
+
+    thicknessInputP = createP('Line Thickness ');
+    thicknessInputP.parent(parentDiv);
+    thicknessInput = createInput('6');
+    thicknessInput.parent(thicknessInputP);
+
+    thicknesLengthToggle = createCheckbox('Line Thickness is Length Dependent ', false);
+    thicknesLengthToggle.parent(parentDiv);
+
+    thicknessLengthSliderP = createP('Thicknes/length factor ');
+    thicknessLengthSliderP.parent(parentDiv);
+    thicknessLengthSlider = createSlider(0, 0.1, 0.05, 0.001);
+    thicknessLengthSlider.parent(thicknessLengthSliderP);
+
+    generateButton = createButton('Generate');
+    generateButton.mousePressed(generateSnowflake);
+    generateButton.parent(parentDiv);
+
+    parentDiv.position(10, 0);
+    parentDiv.hide();
+}
+
+function keyReleased() {
+    console.log(key);
+    switch (key) {
+        case ' ':
+            generateSnowflake();
+            break;
+        case 'M':
+            showMenu = !showMenu;
+            showMenu ? parentDiv.show() : parentDiv.hide();
+            break;
+    }
+}
+
+function generateSnowflake()  {
+	background(0);
+
+    radius = radiusSlider.value() / 2;
+    angleVarianzPIDivider = angleVarianzSlider.value();
+    endLength = endLengthInput.value();
+    thickness = thicknessInput.value();
+    strokeAlpha = strokeAlphaSlider.value();
+    thicknesIsLengthDependent = thicknesLengthToggle.checked();
+    thicknesFactor = thicknessLengthSlider.value();
+
+    let seed = random(255);
+    for (let i = 0; i < symmetrieNumber; ++i) {
+        randomSeed(seed);
+        generateBranch(createVector(width / 2, height / 2), radius, (TWO_PI / symmetrieNumber) * i);
+    }
+}
+
+function generateBranch() {
+	if (length < endLength) {
+        return;
+    }
+
+    let randomAngle = random(0, PI / angleVarianzPIDivider);
+    push();
+    translate(origin.x, origin.y);
+    rotate(angle);
+    generateBranch(createVector(0, 0), length / 2, 0);
+    generateBranch(createVector(length, 0), length / 2, 0);
+    generateBranch(createVector(length / 2, 0), length / 2, -randomAngle);
+    generateBranch(createVector(length / 2, 0), length / 2, randomAngle);
+
+    stroke(255, strokeAlpha);
+    strokeWeight(thicknesIsLengthDependent ? (thickness * (length * thicknesFactor)) : thickness);
+    line(0, 0, length, 0);
+    pop();
+}
